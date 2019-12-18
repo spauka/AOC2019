f6321947fddbc0c0c1316b1119ca49524d06099a
Sebastian Pauka <spauka@gmail.com>
2019-12-19 03:50:08 +1100

    d13

---
+++ d13/d13.py
@@ -0,0 +1,77 @@
+import sys
+from enum import Enum, IntEnum, auto
+sys.path.append("..")
+
+from intcode import IntCode
+from common import qp, Point, Grid
+
+class TileType(IntEnum):
+    EMPTY = 0
+    WALL = 1
+    BLOCK = 2
+    PADDLE = 3
+    BALL = 4
+TileMap = {
+    TileType.EMPTY: " ",
+    TileType.WALL: "█",
+    TileType.BLOCK: "□",
+    TileType.PADDLE: "━",
+    TileType.BALL: "o"
+}
+
+class GameState(Enum):
+    POSX = auto()
+    POSY = auto()
+    TYPE = auto()
+
+class Game(object):
+    def __init__(self, grid):
+        self.grid = grid
+        self.state = GameState.POSX
+        self.x = None
+        self.y = None
+        self.score = None
+        self.ball = None
+        self.paddle = None
+
+    def input(self):
+        if self.ball[0] < self.paddle[0]:
+            return -1
+        elif self.ball[0] > self.paddle[0]:
+            return 1
+        return 0
+    def output(self, val):
+        if self.state is GameState.POSX:
+            self.x = val
+            self.state = GameState.POSY
+        elif self.state is GameState.POSY:
+            self.y = val
+            self.state = GameState.TYPE
+        else:
+            if (self.x, self.y) == (-1, 0):
+                self.score = val
+            else:
+                val = TileType(val)
+                point = Point((self.x, self.y))
+                self.grid[point] = val
+                if val is TileType.BALL:
+                    self.ball = point
+                elif val is TileType.PADDLE:
+                    self.paddle = point
+            self.state = GameState.POSX
+
+with open("input", "r") as inp:
+    init = tuple(qp(inp.read()))
+
+grid = Grid(default=TileType.EMPTY, tilemap=TileMap)
+game = Game(grid)
+p1 = IntCode(init, input_callback=game.input, output_callback=game.output)
+p1.run()
+grid.print_grid()
+print(len(list(tile for tile in grid.tiles.values() if tile == TileType.BLOCK)))
+
+p1.reset()
+p1.mem[0] = 2
+p1.run()
+grid.print_grid()
+print(game.score)
--- d13/intcode_old.py
+++ d13/intcode_old.py
@@ -1,35 +1,29 @@
 import re
 from collections import namedtuple, defaultdict
-from enum import Enum, auto
+from enum import Enum, IntEnum, auto
 from operator import add, mul, lt, eq
 from functools import reduce
-from asyncio import Queue, create_task, run, iscoroutinefunction
-
+from asyncio import run, iscoroutinefunction
 
 def qp(line):
     m = re.findall(r"([-+\d]+)", line)
     return tuple(int(x) for x in m)
 
-
 def compose(*functions):
     return reduce(lambda f, g: lambda *x: f(g(*x)), functions, lambda x: x)
 
-
 Instr = namedtuple("Instr", ("name", "code", "args",
                              "output", "action", "ip_inc"))
-
 
 class States(Enum):
     RESET = auto()
     RUNNING = auto()
     FINISHED = auto()
 
-
 class ArgMode(Enum):
     POS = 0
     IMM = 1
     REL = 2
-
 
 class Memory(list):
     def __init__(self, init):
@@ -48,9 +42,8 @@ class Memory(list):
         except IndexError:
             self.extra_addrs[idx] = val
 
-
 class IntCode(object):
-    def __init__(self, mem, ip=0, input_buffer=None, output_buffer=None):
+    def __init__(self, mem, ip=0, input_callback=None, output_callback=None):
         self.__origmem__ = tuple(mem)
         self.mem = Memory(mem)
         self.ip = ip
@@ -68,10 +61,13 @@ class IntCode(object):
             9: Instr("rbs", 9, 1, False, self.rbs, 2),
             99: Instr("term", 99, 0, False, self.term, 0),
         }
-        self.input_buffer = input_buffer
-        self.output_buffer = output_buffer
+        self.input_callback = input_callback
+        self.output_callback = output_callback
 
-    async def run(self):
+    def run(self):
+        return run(self.async_run())
+
+    async def async_run(self):
         self.state = States.RUNNING
         while self.state is States.RUNNING:
             opcode = self.mem[self.ip] % 100
@@ -124,10 +120,16 @@ class IntCode(object):
 
     # Instrs
     async def input(self):
-        return await self.input_buffer.get()
+        if iscoroutinefunction(self.input_callback):
+            return await self.input_callback()
+        else:
+            return self.input_callback()
 
     async def output(self, arg):
-        await self.output_buffer.put(arg)
+        if iscoroutinefunction(self.output_callback):
+            await self.output_callback(arg)
+        else:
+            self.output_callback(arg)
 
     def rbs(self, offs):
         self.rb += offs
@@ -144,15 +146,109 @@ class IntCode(object):
         else:
             self.ip += 3
 
+class TileType(IntEnum):
+    EMPTY = 0
+    WALL = 1
+    BLOCK = 2
+    PADDLE = 3
+    BALL = 4
+TileMap = {
+    TileType.EMPTY: " ",
+    TileType.WALL: "█",
+    TileType.BLOCK: "□",
+    TileType.PADDLE: "━",
+    TileType.BALL: "o"
+}
 
-with open("input.txt", "r") as inp:
+class Point(tuple):
+    def __add__(self, o):
+        return Point((x+y for x, y in zip(self, o)))
+
+    def __sub__(self, o):
+        return Point((x-y) for x, y in zip(self, o))
+
+    def __abs__(self):
+        return Point(abs(x) for x in self)
+
+class Grid(object):
+    def __init__(self):
+        self.tiles = {}
+        self.tl = Point((0, 0))
+        self.br = Point((0, 0))
+
+    def __getitem__(self, point):
+        if point in self.tiles:
+            return self.tiles[point]
+        return TileType.EMPTY
+
+    def __setitem__(self, point, tiletype):
+        tiletype = TileType(tiletype)
+        self.tiles[point] = tiletype
+        self.tl = Point((min(point[0], self.tl[0]), max(point[1], self.tl[1])))
+        self.br = Point((max(point[0], self.br[0]), min(point[1], self.br[1])))
+
+    def print_grid(self):
+        for y in range(self.br[1], self.tl[1]+1):
+            for x in range(self.tl[0], self.br[0]+1):
+                print(TileMap[self[Point((x, y))]], end="")
+            print()
+
+
+class GameState(Enum):
+    POSX = auto()
+    POSY = auto()
+    TYPE = auto()
+
+class Game(object):
+    def __init__(self, grid):
+        self.grid = grid
+        self.state = GameState.POSX
+        self.x = None
+        self.y = None
+        self.score = None
+        self.ball = None
+        self.paddle = None
+
+    def input(self):
+        if self.ball[0] < self.paddle[0]:
+            return -1
+        elif self.ball[0] > self.paddle[0]:
+            return 1
+        return 0
+    def output(self, val):
+        if self.state is GameState.POSX:
+            self.x = val
+            self.state = GameState.POSY
+        elif self.state is GameState.POSY:
+            self.y = val
+            self.state = GameState.TYPE
+        else:
+            if (self.x, self.y) == (-1, 0):
+                self.score = val
+            else:
+                val = TileType(val)
+                point = Point((self.x, self.y))
+                self.grid[point] = val
+                if val is TileType.BALL:
+                    self.ball = point
+                elif val is TileType.PADDLE:
+                    self.paddle = point
+            self.state = GameState.POSX
+
+
+with open("input", "r") as inp:
     init = tuple(qp(inp.read()))
 
 
-input_buffer = Queue()
-output_buffer = Queue()
-input_buffer.put_nowait(1)
-p1 = IntCode(init, input_buffer=input_buffer, output_buffer=output_buffer)
-run(p1.run())
-while not output_buffer.empty():
-    print(output_buffer.get_nowait())
+grid = Grid()
+game = Game(grid)
+p1 = IntCode(init, input_callback=game.input, output_callback=game.output)
+p1.run()
+grid.print_grid()
+print(len(list(tile for tile in grid.tiles.values() if tile == TileType.BLOCK)))
+
+p1.reset()
+p1.mem[0] = 2
+p1.run()
+grid.print_grid()
+print(game.score)
